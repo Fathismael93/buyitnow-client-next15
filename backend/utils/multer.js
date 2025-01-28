82484e798ff586756920339bb2424310e75cccb5
Fathismael93 <fathismael@gmail.com>
2025-01-27 22:11:44 -0800

    Checking if profile picture is present in formdata

--- backend/utils/multer.js
+++ backend/utils/multer.js
@@ -1,21 +1,27 @@
-import multer from "multer";
+import multer from 'multer';
 
 const storage = multer.diskStorage({
   filename: function (req, file, cb) {
-    cb(null, new Date().toDateString() + "-" + file.originalname);
+    cb(null, new Date().toDateString() + '-' + file.originalname);
+
+    console.log('File Name');
+    console.log(file.originalname);
   },
 });
 
 const fileFilter = (req, file, cb) => {
   if (
-    file.mimetype === "image/jpeg" ||
-    file.mimetype === "image/png" ||
-    file.mimetype === "image/jpg"
+    file.mimetype === 'image/jpeg' ||
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg'
   ) {
     file;
+
+    console.log('File is present here');
+
     cb(null, true);
   } else {
-    ({ error: "Unsupported file format. Upload only JPEG/JPG or PNG" }), false;
+    ({ error: 'Unsupported file format. Upload only JPEG/JPG or PNG' }), false;
   }
 };
 

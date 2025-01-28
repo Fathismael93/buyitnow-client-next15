2d2acbd5af0145fb0193873b2693b4a099f7d429
Fathismael93 <fathismael@gmail.com>
2025-01-28 06:53:49 -0800

    First Improve of myOrders page: forgot to add h2

--- app/me/layout.jsx
+++ app/me/layout.jsx
@@ -14,7 +14,7 @@ export default function UserLayout({ children }) {
     <>
       <section className="flex flex-row py-3 sm:py-7 bg-blue-100">
         <div className="container max-w-screen-xl mx-auto px-4">
-          <h2 className="font-medium text-2xl">{user?.name?.toUpperCase()}</h1>
+          <h2 className="font-medium text-2xl">{user?.name?.toUpperCase()}</h2>
         </div>
         <div className="md:hidden mx-4">
           <button

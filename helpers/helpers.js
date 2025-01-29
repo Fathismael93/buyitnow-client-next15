f137ae8e4054c703087cb9a3192ad0783bb78310
Fathismael93 <fathismael@gmail.com>
2025-01-28 22:38:01 -0800

    Exporting customLoader in default mode

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -31,7 +31,7 @@ export const getCookieName = () => {
 };
 
 // Demo: https://res.cloudinary.com/demo/image/upload/w_300,c_limit,q_auto/turtles.jpg
-export function cloudinaryLoader({ src, width, quality }) {
+export default function cloudinaryLoader({ src, width, quality }) {
   const params = ['f_auto', 'c_limit', `w_${width}`, `q_${quality || 'auto'}`];
   return `https://example.com/${params.join(',')}${src}`;
 }

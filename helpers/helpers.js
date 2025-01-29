c1d78c364531de329066d079342ec265ddc90cd9
Fathismael93 <fathismael@gmail.com>
2025-01-28 22:41:41 -0800

    Config customLoader

--- helpers/helpers.js
+++ helpers/helpers.js
@@ -33,7 +33,7 @@ export const getCookieName = () => {
 // Demo: https://res.cloudinary.com/demo/image/upload/w_300,c_limit,q_auto/turtles.jpg
 export default function cloudinaryLoader({ src, width, quality }) {
   const params = ['f_auto', 'c_limit', `w_${width}`, `q_${quality || 'auto'}`];
-  return `https://example.com/${params.join(',')}${src}`;
+  return `${params.join(',')}${src}`;
 }
 
 export const arrayHasData = (array) => {

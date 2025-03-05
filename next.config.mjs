9c3f82f989e5b7ba8aeae7f8d6fd953feff2d0ee
Fathismael93 <fathismael@gmail.com>
2025-03-05 11:31:13 -0800

    Replaced all LazyLoadingImage comp with next Image comp and handled image choosin in ProductDetails with useState

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'http',
+        protocol: 'https',
         hostname: 'res.cloudinary.com',
         port: '',
         pathname: '**',

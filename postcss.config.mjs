6d845b7cd93aea9ec3f54a28d2b44988200b6c5c
Fathismael93 <fathismael@gmail.com>
2025-03-06 21:53:42 -0800

    Issue with postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -28,19 +28,19 @@ const config = {
           },
 
           // Supprimer les styles inutilisés
-          '@fullhuman/postcss-purgecss': {
-            content: [
-              './pages/**/*.{js,jsx,ts,tsx}',
-              './components/**/*.{js,jsx,ts,tsx}',
-              './app/**/*.{js,jsx,ts,tsx}',
-            ],
-            // defaultExtractor: (content) =>
-            //   content.match(/[\w-/:]+(?<!:)/g) || [],
-            safelist: {
-              standard: ['html', 'body', /^react-/, /^next-/],
-              deep: [/toast/, /lazy-load/],
-            },
-          },
+          // '@fullhuman/postcss-purgecss': {
+          //   content: [
+          //     './pages/**/*.{js,jsx,ts,tsx}',
+          //     './components/**/*.{js,jsx,ts,tsx}',
+          //     './app/**/*.{js,jsx,ts,tsx}',
+          //   ],
+          //   defaultExtractor: (content) =>
+          //     content.match(/[\w-/:]+(?<!:)/g) || [],
+          //   safelist: {
+          //     standard: ['html', 'body', /^react-/, /^next-/],
+          //     deep: [/toast/, /lazy-load/],
+          //   },
+          // },
         }
       : {}),
   },
